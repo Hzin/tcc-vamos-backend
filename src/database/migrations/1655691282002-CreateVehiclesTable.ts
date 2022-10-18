ffa57df76a2f87dcc8f650614fc6a449b6506fe6
Matheus Albino Brunhara <matheusalb3213@gmail.com>
2022-10-18 20:48:45 -0300

    Melhora migrations

--- src/database/migrations/1655691282002-CreateVehiclesTable.ts
+++ src/database/migrations/1655691282002-CreateVehiclesTable.ts
@@ -1,4 +1,4 @@
-import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+import { MigrationInterface, QueryRunner, Table, TableForeignKey } from 'typeorm';
 
 export class CreateVehiclesTable1655691282002 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
@@ -70,9 +70,26 @@ export class CreateVehiclesTable1655691282002 implements MigrationInterface {
         ],
       }),
     );
+
+    await queryRunner.createForeignKey(
+      'vehicles',
+      new TableForeignKey({
+        name: 'UserIdVehicle', // nome da FK, serve para referenciar numa exclusão pelo QueryRunner se necessário
+        columnNames: ['user_id'], // coluna que vai virar FK
+        referencedColumnNames: ['id_user'], // coluna PK da primeira tabela
+        referencedTableName: 'users', // nome da tabela que possui a PK
+        onDelete: 'SET NULL',
+        onUpdate: 'CASCADE',
+      }),
+    );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropForeignKey(
+      'vehicles',
+      'UserIdVehicle',
+    );
+
     await queryRunner.dropTable('vehicles');
   }
 }
