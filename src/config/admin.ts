59a6150c5c437a9e190c3bce8531c7a37e6e9fee
Hugo Falcao <hugocoelhof03@gmail.com>
2022-12-05 17:58:48 -0300

    refactor: :recycle: wip

--- src/config/admin.ts
+++ src/config/admin.ts
@@ -1,6 +1,7 @@
 export default {
   key: 'b0df76b7fdf1823196f935f0417c0ad3',
   users: [
-    '3cfd8765-2c90-423d-a49b-f8b00dc39f25'
+    '3cfd8765-2c90-423d-a49b-f8b00dc39f25',
+    '0bfcb062-5b71-44ef-a01c-f0f89481b8fd'
   ]
 };
--- src/services/User/CreateUserSearchingService.ts
+++ src/services/User/CreateUserSearchingService.ts
@@ -12,18 +12,34 @@ interface Request {
   longitude_from: string;
   latitude_to: string;
   longitude_to: string;
-  address_to: string
+  address_to: string;
 }
 
 class CreateUserSearchingService {
-  public async execute({ id_user, latitude_from, longitude_from, latitude_to, longitude_to, address_to }: Request): Promise<UserSearching> {
+  public async execute({
+    id_user,
+    latitude_from,
+    longitude_from,
+    latitude_to,
+    longitude_to,
+    address_to,
+  }: Request): Promise<UserSearching> {
     const usersSearchingRepository = getRepository(UserSearching);
 
-    const findUserService = new FindUserService()
-    const user = await findUserService.execute(id_user)
+    const findUserService = new FindUserService();
+    const user = await findUserService.execute(id_user);
+
+    if (!user) {
+      throw new AppError('User not found.', 404);
+    }
 
     const search = usersSearchingRepository.create({
-      user, latitude_from, longitude_from, latitude_to, longitude_to, address_to
+      user,
+      latitude_from,
+      longitude_from,
+      latitude_to,
+      longitude_to,
+      address_to,
     });
     await usersSearchingRepository.save(search);
 
